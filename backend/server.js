64374e3dd8c661f684c439454a54b0cc3f3a7e8f
Rajeev.v <rajeevvalechha@gmail.com>
2025-08-20 19:38:41 +0530

    Completed Authorization and Session - Backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const sessionRoutes = require("./routes/sessionRoutes");
 
 const app = express();
 
@@ -14,13 +18,21 @@ app.use(
   })
 );
 
+connectDB();
+
 // Middleware
 app.use(express.json());
 
 // Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/sessions", sessionRoutes);
+// app.use("/api/questions", questionRoutes);
+
+// app.use("api/ai/generate-questions", protect, generateInterviewQuestions);
+// app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 
 // Serve uploads folder
-app.use("/uploades", express.static(path.join(__dirname, "uploades"), {}));
+app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
 // Start Server
 const PORT = process.env.PORT || 5000;
