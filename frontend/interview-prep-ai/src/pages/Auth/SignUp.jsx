943566e77057396a2c2b64952f1e2e40039d7f24
Rajeev.v <rajeevvalechha@gmail.com>
2025-08-29 20:26:52 +0530

    Dashboard completed

--- frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
+++ frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Input from "../../components/Inputs/Input";
 import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector";
 import { validateEmail } from "../../utils/helper";
+import { UserContext } from "../../context/userContext";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import uploadImage from "../../utils/uploadImage";
 
 const SignUp = ({ setCurrentPage }) => {
   const [profilePic, setProfilePic] = useState(null);
@@ -12,6 +16,7 @@ const SignUp = ({ setCurrentPage }) => {
 
   const [error, setError] = useState(null);
 
+  const { updateUser } = useContext(UserContext);
   const navigate = useNavigate();
 
   // handle SignUp Form submission
@@ -39,10 +44,31 @@ const SignUp = ({ setCurrentPage }) => {
 
     // SignUp API CALL
     try {
+      // Upload image if present
+      if (profilePic) {
+        const imageUploadesRes = await uploadImage(profilePic);
+        profileImageUrl = imageUploadesRes.imageUrl || "";
+      }
+
+      const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+        name: fullName,
+        email,
+        password,
+        profileImageUrl,
+      });
+
+      const { token } = response.data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+        updateUser(response.data);
+        navigate("/dashboard");
+      }
     } catch (error) {
       if (error.response && error.response.data.message) {
         setError(error.response.data.message);
       } else {
+        console.error(error);
         setError("An unexpected error occurred. Please try again.");
       }
     }
--- frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
+++ frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import moment from "moment";
+import { AnimatePresence, motion } from "framer-motion";
+import { LuCircleAlert, LuListCollapse } from "react-icons/lu";
+import SpinnerLoader from "../../components/Loader/SpinnerLoader";
+import { toast } from "react-hot-toast";
 
 const InterviewPrep = () => {
+  const { sessionId } = useParams();
+
+  const [sessionData, setSessionData] = useState(null);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const [openLearnMoreDrawer, setOpenLearnMoreDrawer] = useState(false);
+  const [explanation, setExplanation] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isUpdateLoader, setIsUpdateLoader] = useState(false);
   return <div>InterviewPrep</div>;
 };
 
